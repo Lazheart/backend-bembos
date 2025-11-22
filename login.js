99670bf68e77d627613dfe458c84dff9d90a0768
Lazheart <176245760+Lazheart@users.noreply.github.com>
2025-11-22 10:33:18 -0500

    infra(orders): add OrdersTable and OrdersBucket resources; add authorizer function and protect orders endpoints; add ORDERS env vars

--- login.js
+++
@@ -1,4 +0,0 @@
-// DEPRECATED: use auth/login.js
-module.exports = function deprecated() {
-  throw new Error('login.js is deprecated. Use auth/login.js');
-};
--- register.js
+++
@@ -1,4 +0,0 @@
-// DEPRECATED: use auth/register.js
-module.exports = function deprecated() {
-  throw new Error('register.js is deprecated. Use auth/register.js');
-};
